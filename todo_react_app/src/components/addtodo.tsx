ff2fc97d54914d68a99258d534d8d9e35da9da7b
unknown <muktinathrajbanshi786@gmail.com>
2026-02-27 20:44:31 +0545

    wow maja airachha yar

---
+++ todo_react_app/src/components/addtodo.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const AddToDo = () => {
+
+    const [todo, setTodo] = useState("";)
+
+  return (
+    <form>
+        <input type="text" value={} onChange={} />
+    </form>
+  )
+}
+
+export default AddToDo
