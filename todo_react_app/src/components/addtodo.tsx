82801976c696e5fc34c8f903755f143301fdf567
unknown <muktinathrajbanshi786@gmail.com>
2026-02-27 21:07:44 +0545

    wow what a logic man

--- todo_react_app/src/components/addtodo.tsx
+++ todo_react_app/src/components/addtodo.tsx
@@ -4,8 +4,15 @@ const AddToDo = () => {
 
     const [todo, setTodo] = useState("");
 
+
+    const handleFormSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        handleAddToDo(todo);
+        setTodo("");
+    }
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
         <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
         <button type="submit">Add</button>
     </form>
