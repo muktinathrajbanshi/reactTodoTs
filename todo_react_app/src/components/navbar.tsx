acd248c624f0fb2ee9aeaac75d2822095b48a116
unknown <muktinathrajbanshi786@gmail.com>
2026-03-03 19:49:09 +0545

    jahilai chadai sakum vanchhu feri dhilo vai halchha k saro busy yar

--- todo_react_app/src/components/navbar.tsx
+++ todo_react_app/src/components/navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 const Navbar = () => {
+    const [searchParams] = useSearchParams();
+    let todosData = searchParams.get("todos");
   return (
     <nav>
       <Link to="/" className=""> All </Link>
-      <Link to="/?todos=active" className=""> Active </Link>
+      <Link to="/?todos=active" className= {todosData === "active" ? "active" : ""}> Active </Link>
       <Link to="/?todos=completed" className=""> Completed </Link>
 
     </nav>
